a817aa7a4918e53fdc92c4354c4423448816e74b
Ben <benlewis1994@gmail.com>
2017-12-09 18:55:57 +0000

    Updated setup to install vim plugins silently

--- setup.sh
+++ setup.sh
@@ -30,5 +30,7 @@ ln -fs $(pwd)/zsh_aliases ~/.zsh_aliases
 ln -fs $(pwd)/zsh_paths ~/.zsh_paths
 ln -fs $(pwd)/zshrc ~/.zshrc
 
+vim +PluginInstall +qall
 echo "You will be prompted for your password to switch shells"
 chsh -s /bin/zsh `whoami`
+
